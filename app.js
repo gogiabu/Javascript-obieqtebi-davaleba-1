c155e45aeb8b74f2894b1a0dd7a74790b3e5caf4
gogiabu <146556307+gogiabu@users.noreply.github.com>
2024-05-14 11:43:09 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var object = {
+    სახელი : "გოგი" ,
+    გვარი  : "აბუსელიძე",
+    ასაკი  : 33,
+    სიმაღლე : 170
+};
+document.write ( object.სახელი + ("<br>") );
+document.write ( object.გვარი + ("<br>") );
+document.write ( object.ასაკი + ("<br>") );
+document.write ( object.სიმაღლე + ("<br>") );
